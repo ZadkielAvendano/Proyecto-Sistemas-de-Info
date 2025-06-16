b87df2f2198d3662afc833a5c7dc79c31ffdc78c
Miguelangel Maiz <miguelangel.maiz@correo.unimet.edu.ve>
2025-06-15 20:50:07 -0400

    Pruebas de porque no funciona

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,14 @@
-
+import { Link } from "react-router";
 
 export default function Navbar() {
   return (
-    <ul>
+    <nav>
+        <ul>
             <li><link to='/'>Homepage</link></li>
             <li><link to='/register'>Register</link></li>
             <li><link to='/login'>Login</link></li>
             <li><link to='/profile'>Profile</link></li>
         </ul>
+    </nav>
   )
 }
