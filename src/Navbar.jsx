369cac6ad17ba0e91d1c778f46447739a9e6a512
Zadkiel Avendano <zadkiel4600@gmail.com>
2025-06-16 18:56:28 -0400

    Implementacion de Header

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router";
+import unimetLogo from "./assets/unimet_logo.png";
 
 export default function Navbar() {
   return (
-    <nav>
+    <header className="header">
+      <div className="logo">
+        <Link to="/"><img src={unimetLogo} alt="Unimet Logo" /></Link>
+      </div>
+      <nav className="navbar">
         <ul>
-            <li><Link to='/'>Homepage</Link></li>
-            <li><Link to='/register'>Register</Link></li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/profile'>Profile</Link></li>
+          <li><Link>Espacios</Link></li>
+          <li><Link>Calendarios</Link></li>
+          <li><Link>Reservas</Link></li>
+          <li><Link>Contacto</Link></li>
+          <li id="login_button"><Link to='/login'><button>Iniciar Sesion</button></Link></li>
+          <li id="register_button" className="secondary_button"><Link to='/register'><button>Registrarse</button></Link></li>
+          <li id="profile_button"><Link to='/profile'><button>Perfil</button></Link></li>
         </ul>
-    </nav>
+      </nav>
+    </header>
   )
 }
